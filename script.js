f28d40fc4d665912d8680d9a81993042a3dc0356
regeneratecelulas <regeneratecelulasmadre@gmail.com>
2026-04-04 09:29:46 -0600

    fidi

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const allHotspots = document.querySelectorAll('.hotspot');
     const legendListItems = document.querySelectorAll('.legend-list li');
 
+    const isMetaBrowser = /FBAN|FBAV|Instagram/i.test(navigator.userAgent);
+    if (isMetaBrowser) {
+    document.body.classList.add('meta-browser');
+    }
+
     if (!toggleButton || !mapOriginal || !mapContainer) return;
 
     // --- NUEVA LÓGICA: CARGA BAJO DEMANDA (Lazy Load Manual) ---
